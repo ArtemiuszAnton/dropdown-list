97bf2acb5a77087b69f1ecaaaa3ef696d912acf4
ArtemiuszAnton <elvispresley9122@gmail.com>
2023-10-07 12:26:20 +0200

    test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const inp = document.querySelector('input');
 const select = document.querySelector('.select');
 const block = document.querySelector('.block');
 const arrow = document.querySelector('.img-arrow');
+
 let flag = false;
 
 
@@ -18,7 +19,27 @@ select.addEventListener('click', () => {
 })
 
 block.addEventListener('click', (event) => {
+    inp.value = event.target.textContent;
+    block.style = 'visibility:hidden';
+    flag = false;
+    arrow.style = 'background-image:url(./assets/arrowdown.svg)'
 
 })
 
+// select.addEventListener('dblclick', () => {
+//     inp.removeAttribute('disabled')
+// })
 
+inp.addEventListener('keydown', (event) => {
+    try {
+        if (event.key == 'Enter') {
+            if (!inp.value) throw new Error('input is empty')
+            const p = document.createElement('p');
+            block.appendChild(p);
+            p.textContent = inp.value
+        }
+    } catch (error) {
+        alert(error.message)
+    }
+
+})
